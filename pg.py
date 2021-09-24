0c4505b52d9f7aa305898fa24d103256ea46d39c
Jerome Caruso <jerome0caruso@gmail.com>
2021-09-24 14:11:13 -0500

    Almost donegit add .

--- pg.py
+++ pg.py
@@ -1,10 +1,11 @@
 
 class ParkingGarage():
 
-    def __init__(self, tickets = 50, parkingSpaces = [], currentTicket = {}):
+    def __init__(self, tickets = 50, parkingSpaces = [], currentTicket = {}, eachTicket = 0):
         self.tickets = tickets
         self.parkingSpaces = parkingSpaces
         self.currentTicket = currentTicket
+        self.eachTicket = eachTicket
 
     def parkingSpacelogic(self):
         for space in range(0, 50):
@@ -14,21 +15,33 @@ class ParkingGarage():
         
         self.tickets -= 1
         print(f"Tickets left: {self.tickets}")
-        if len(self.parkingSpaces) <=1:
+        if len(self.parkingSpaces) <= 1:
             print("There are no more spots left!")
         else:
             self.parkingSpaces.pop()
-            print("taking ticket")
+            print(f"taking ticket #{self.tickets + 1}")
             print(f"Parking Spaces left: {len(self.parkingSpaces)}")
+            self.currentTicket[self.tickets + 1] = {'Paid': False}
 
     def payForParking(self):
-        print("You are paying")
-        pass
+        while True:
+            payment = input("Did you enter money? Y/N: ").lower()
+            if payment == 'y':
+                self.eachTicket = int(input("What ticket would you like to pay for?: "))
+                self.currentTicket[self.eachTicket] = {'Paid': True}
+                print(f"You ticket #{self.eachTicket} has been paid and you have 15 minutes to leave!")
+                break
+            print("Please enter money!!!")
 
     def leaveGarage(self):
-        print("Bye you are exiting!")
-        pass
+        driver_ticket = int(input("What ticket did you have?: "))
 
+        if self.currentTicket[driver_ticket]["Paid"] == True:
+             print("Thank you, have a nice day!")
+             return True
+        else:
+            print("You must go back and pay please!")
+                
 def driver():
     driver_1 = ParkingGarage()
     driver_1.parkingSpacelogic()
@@ -37,9 +50,13 @@ def driver():
         if driver_input == 'take':
             driver_1.takeTicket()
             driver_input = input("What would you like to do? type 'take' to take a ticket, type 'pay' to pay a for parking or type 'leave' to exit the garage: ").lower().strip()
-        if driver_input == 'pay':
-            driver_1.payForParking()
-            driver_input = input("What would you like to do? type 'take' to take a ticket, type 'pay' to pay a for parking or type 'leave' to exit the garage: ").lower().strip()
+        if  driver_input == 'pay':
+            if driver_1.tickets == 50:
+                print("No tickets have been taken!")
+                driver_input = input("What would you like to do? type 'take' to take a ticket, type 'pay' to pay a for parking or type 'leave' to exit the garage: ").lower().strip()
+            else:    
+                driver_1.payForParking()
+                driver_input = input("What would you like to do? type 'take' to take a ticket, type 'pay' to pay a for parking or type 'leave' to exit the garage: ").lower().strip()
         if driver_input == 'leave':
             driver_1.leaveGarage()
             driver_input = input("What would you like to do? type 'take' to take a ticket, type 'pay' to pay a for parking or type 'leave' to exit the garage: ").lower().strip()
